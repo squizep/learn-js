422b6f84eb9d3b47cf31182cb8d96492feedcd66
squizep <wonderbuf@gmail.com>
2022-07-08 16:41:35 +0300

    yes

--- src/main.js
+++ src/main.js
@@ -1,41 +1,11 @@
-/*class Comment {
-	constructor(text) {
-		this.text = text,
-		this.votesQty = 0
-	}
+const timerPromise =  () => 
+	new Promise((resolve, reject) =>
+		setTimeout(() => resolve(), 2000))
 
-	upvote() {
-		this.votesQty += 1;
-	}
-
-	static mergeComments(first, second) {
-		return `${first} ${second}`;
-	}
+const asyncFn = async () => {
+	console.log('Timer starts')
+	await timerPromise()
+	console.log('Timer ended');
 }
-const firstComment = new Comment('First comment');
-const secondComment = new Comment('Second comment');
-const thirdComment = new Comment('Third comment');
-firstComment.upvote();
-firstComment.upvote();
-firstComment.upvote();
-console.log(firstComment);
-secondComment.upvote();
-secondComment.upvote();
-console.log(secondComment);
-thirdComment.upvote();
-console.log(thirdComment);
-Comment.mergeComments('first comment', 'second comment');
-*/
 
-/*class NumberArray extends Array {
-	sum() {
-		return this.reduce((el, acc) => acc + el);
-	}
-}
-const myArray = new NumberArray(2, 5, 7, 8, 15);
-console.log(myArray); */
-
-fetch('https://jsonplaceholder.typicode.com/todos/1')
-	.then((response) => response.json())
-	.then((json) => console.log(json))
-	.catch((error) => console.error(error));
+asyncFn()
--- src/mainTwo.js
+++ src/mainTwo.js
@@ -4,8 +4,4 @@ const getData = (url) =>
 			.then(response => response.json())
 			.then(json => resolve(json))
 			.catch(error => reject(error))
-);
-
-getData('https://jsonplaceholder.typicode.com/todos/1')
-	.then(data => console.log(data))
-	.catch(error => console.log(error.message))
+)
