b7728031ca8b5b41f020a2a92ae5983b3d42e1b1
squizep <wonderbuf@gmail.com>
2022-07-09 13:52:34 +0300

    yes

--- src/main.js
+++ src/main.js
@@ -1,9 +1,26 @@
-const timerPromise = () =>
+/*const timerPromise = () =>
 	new Promise((resolve, reject) => 
 		setTimeout(() => resolve(), 2000))
 
 const asyncFn = async () => {
 	console.log('Timer starts');
+	const startTime = performance.now() 
 	await timerPromise()
-	console.log('Timer ended');
+	const endTime = performance.now()
+	console.log('Timer ended', endTime - startTime);
+}
+asyncFn() */
+
+const getData = async (url) => {
+	const res = await fetch(url);
+	const json = await res.json();
+	return json;
+};
+
+const url = 'https://jsonplaceholder.typicode.com/todos/';
+try {
+	const data = await getData(url);
+	console.log(data);
+} catch (error) {
+	console.error(error.message);
 }
